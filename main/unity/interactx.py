ebd113e214972fe9747d44794ad6658a0de1a5f7
thedtvn <duongtuan30306@gmail.com>
2023-07-09 17:20:07 +0700

    update  for  rate  limit  faster

--- main/unity/interactx.py
+++ main/unity/interactx.py
@@ -15,22 +15,25 @@ async def check(userid):
     rate_limit[str(userid)] = old_id - 1
     if rate_limit[str(userid)] < 1:
         del rate_limit[str(userid)]
+        
+async def ratelimit_check(obj):
+    userid = obj.user.id
+    old_id = rate_limit.get(str(userid), 0)
+    rate_limit[str(userid)] = old_id + 1
+    with database(f"./data/ratelimit", obj.client.db) as db:
+        if rate_limit.get(str(userid), 0) >= 3 or db.get(str(obj.user.id)):
+            del rate_limit[str(userid)]
+            db[str(obj.user.id)] = True
+            return True
+    asyncio.create_task(check(userid))
+    return False
 
 async def Interactx(obj: discord.Interaction, *, ephemeral: bool = False, start: bool = True) -> commands.Context:
     if type(obj) is discord.Interaction:
         newctx = await obj.client.get_context(obj)
         userid = obj.user.id
         if start:
-            old_id = rate_limit.get(str(userid), 0)
-            rate_limit[str(userid)] = old_id + 1
-            with database(f"./data/ratelimit", obj.client.db) as db:
-                if rate_limit.get(str(userid), 0) >= 3 or db.get(str(obj.user.id)):
-                    del rate_limit[str(userid)]
-                    db[str(obj.user.id)] = True
-                    raise CommandRateLimit()
-                else:
-                    await newctx.defer(ephemeral=ephemeral)
-            asyncio.create_task(check(userid))
+            await newctx.defer(ephemeral=ephemeral)
     else:
         newctx = obj
     return newctx
